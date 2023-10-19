4cb1bc32cea64d10fec24b812201aebad379b26c
Tigran <127849741+Bianconeri21@users.noreply.github.com>
2023-10-19 15:09:44 +0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <AudioPlayer/>
+</template>
+
+<script>
+import AudioPlayer from '@/components/AudioPlayer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AudioPlayer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
